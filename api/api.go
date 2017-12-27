70ca9d0f5a6b79060765fa77ed6e86b63494bc4f
cappyzawa <m153304w@st.u-gakugei.ac.jp>
2017-12-28 05:31:48 +0900

    enable to read users & a user

--- api/api.go
+++ api/api.go
@@ -1,7 +1,64 @@
 package api
 
-import "net/http"
+import (
+	"net/http"
+	"log"
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"github.com/cappyzawa/go-tiny-crud/db"
+)
 
-func Hello(w http.ResponseWriter, r *http.Request) {
-	println("hello")
+type User struct {
+	Id   string	`json: "id"`
+	Name string	`json: "name"`
+	Ruby string `json: "ruby"`
+}
+
+var (
+	user User
+	users []User
+)
+
+func ListUsers(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+	users = GetUsers(users)
+	json.NewEncoder(w).Encode(users)
+}
+
+func UserById(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+	user := GetUser(params.ByName("id"))
+	json.NewEncoder(w).Encode(user)
+}
+
+func GetUser(queryId string) User{
+	mysql := db.ConnectMysql()
+	defer mysql.Close()
+
+	notFoundErr := mysql.QueryRow(`SELECT * FROM users WHERE id = ?`, queryId).Scan(&user.Id, &user.Name, &user.Ruby)
+	if notFoundErr != nil {
+		log.Fatal("not found user by " + queryId)
+	}
+
+	return user
+}
+
+func GetUsers(users []User) []User{
+	mysql := db.ConnectMysql()
+	defer mysql.Close()
+
+	rows, err := mysql.Query("SELECT * FROM users")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next(){
+		user := User{}
+		err = rows.Scan(&user.Id, &user.Name, &user.Ruby)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	return users
 }
--- main.go
+++ main.go
@@ -4,17 +4,19 @@ import (
 	"net/http"
 	"log"
 	"github.com/cappyzawa/go-tiny-crud/api"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	//routingはここ
-	http.HandleFunc("/", api.Hello)
-	//この下にrouting定義を増やしていけばいい感じな気がする。
+	router := httprouter.New()
+	router.GET("/users", api.ListUsers)
+	router.GET("/users/:id", api.UserById)
 
 	log.Printf("Start Go HTTP Server")
 
 	//port監視と実行
-	err := http.ListenAndServe(":9090", nil)
+	err := http.ListenAndServe(":18081", router)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
