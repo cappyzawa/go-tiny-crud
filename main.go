4dd54ad0d14d5c43fac8fc4b561e22cf74fbdf67
Shu Kutsuzawa <m153304w@st.u-gakugei.ac.jp>
2018-01-01 03:40:03 +0900

    Revert "change routing lib httprouter -> echo (#5)" (#6)
    This reverts commit 854e28a3d87c0e6c384e9f3a0489c788d55feb5f.

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"net/http"
+	"log"
+	"github.com/cappyzawa/go-tiny-crud/api"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/cappyzawa/go-tiny-crud/router"
+	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
-	r := router.NewRouter()
-	r.Logger.Fatal(r.Start(":18081"))
+	router := httprouter.New()
+	router.GET("/users", api.ListUsers)
+	router.GET("/users/:id", api.UserById)
+
+	log.Printf("Start Go HTTP Server")
+
+	//port監視と実行
+	err := http.ListenAndServe(":18081", router)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
